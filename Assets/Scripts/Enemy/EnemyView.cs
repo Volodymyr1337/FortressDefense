812529fbd9939777b226c8d7f3e2275089b6053d
docent <docentkafedru@gmail.com>
2018-07-13 16:31:19 +0300

    refactoring

--- Assets/Scripts/Enemy/EnemyView.cs
+++ Assets/Scripts/Enemy/EnemyView.cs
@@ -56,30 +56,3 @@ public class EnemyView : UnitView
         stateMachine.ChangeState(new DieState(this));
     }
 }
-
-
-public class EnemyWalkState : IState
-{
-    private EnemyView owner;
-    private float speed;
-
-    public EnemyWalkState(EnemyView owner, float speed)
-    {
-        this.owner = owner;
-        this.speed = speed;
-    }
-
-    public void Enter()
-    {
-        owner.GetComponent<Animator>().SetTrigger("Walk");        
-    }
-
-    public void Execute()
-    {
-        owner.transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
-    }
-
-    public void Exit()
-    {
-    }
-}
---
+++ Assets/Scripts/States/EnemyWalkState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyWalkState : IState
+{
+    private EnemyView owner;
+    private float speed;
+
+    public EnemyWalkState(EnemyView owner, float speed)
+    {
+        this.owner = owner;
+        this.speed = speed;
+    }
+
+    public void Enter()
+    {
+        owner.GetComponent<Animator>().SetTrigger("Walk");
+    }
+
+    public void Execute()
+    {
+        owner.transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
+    }
+
+    public void Exit()
+    {
+    }
+}
